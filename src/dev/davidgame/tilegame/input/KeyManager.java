1443a9aa776ca02d1ecf5b726dce9c53e709f235
David Kennedy <david01181998@gmail.com>
2020-02-18 21:46:41 -0500

    Added functionality to KeyManager. Now holding key does is different than seperate presses

--- src/dev/davidgame/tilegame/input/KeyManager.java
+++ src/dev/davidgame/tilegame/input/KeyManager.java
@@ -5,16 +5,33 @@ import java.awt.event.KeyListener;
 
 public class KeyManager implements KeyListener {
 	
-	private boolean[] keys;
+	private boolean[] keys, justPressed, cantPress;
 	public boolean up, down, left, right; //Movement
 	public boolean n_attack; //Normal attack
 	public boolean pick_up; //Pick Up Items
 	
 	public KeyManager() {
 		keys = new boolean[256];
+		justPressed = new boolean[keys.length];
+		cantPress = new boolean[keys.length];
 	}
 	
 	public void tick() {
+		
+		for (int i = 0; i < keys.length; i++) {
+			// Loop through each key code. Allows key holding or key
+			if(cantPress[i] == true && !keys[i]) {
+				//If cantPress is true but key is no longer active
+				cantPress[i] = false; //Key is now press able
+			} else if(justPressed[i]){
+				cantPress[i] = true; //Can not press until key is released
+				justPressed[i] = false;
+			}
+			if(!cantPress[i] && keys[i]) {
+				justPressed[i] = true;
+			}
+		}
+		
 		up = keys[KeyEvent.VK_UP];
 		down = keys[KeyEvent.VK_DOWN];
 		left = keys[KeyEvent.VK_LEFT];
@@ -22,6 +39,13 @@ public class KeyManager implements KeyListener {
 		
 		n_attack = keys[KeyEvent.VK_Q];
 		pick_up = keys[KeyEvent.VK_F];
+	}
+	
+	public boolean keyJustPressed(int keyCode) {
+		if(keyCode <0 || keyCode >= keys.length) {
+			return false;
+		}
+		return justPressed[keyCode];
 	}
 
 	@Override
